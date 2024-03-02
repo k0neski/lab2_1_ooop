fafb0a6412302e3389fa5a0b3e7f8948c643168c
k0neski <125816238+k0neski@users.noreply.github.com>
2024-03-02 15:16:19 +0300

    Update lab2_1.java

--- lab2_1.java
+++ lab2_1.java
@@ -2,38 +2,20 @@ import java.util.Scanner;
 
 public class lab2_1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("a:");
-        double a = scanner.nextDouble();
-
-        System.out.println("b:");
-        double b = scanner.nextDouble();
-
-        System.out.println("e:");
-        double e = scanner.nextDouble();
-
-        System.out.println("x1:");
-        double x1 = scanner.nextDouble();
-
-        System.out.println("x2:");
-        double x2 = scanner.nextDouble();
-
-        double y1, y2;
-
-        if (x1 <= 1) {
-            y1 = 2 * x1 * x1 + a * Math.cos(b * x1);
+        Scanner input = new Scanner(System.in);
+        System.out.print("x = ");
+        int x = input.nextInt();
+        int e = 2;
+        double res;
+        if(x > 0){
+            System.out.print("a = ");
+            int a = input.nextInt();
+            System.out.print("b = ");
+            int b = input.nextInt();
+            res = 2 * x * x + a * Math.cos(b * x);
         } else {
-            y1 = e + Math.tan(Math.pow(x1, 3));
+            res = e + Math.tan(Math.pow(x, 3));;
         }
-
-        if (x2 <= 1) {
-            y2 = 2 * x2 * x2 + a * Math.cos(b * x2);
-        } else {
-            y2 = e + Math.tan(Math.pow(x2, 3));
-        }
-
-        System.out.printf("f(%f) = %f%n", x1, y1);
-        System.out.printf("f(%f) = %f%n", x2, y2);
+        System.out.println(String.format("%.2f", res));
     }
 }
